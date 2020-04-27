4eae3f9f1a39a4e07244142f83cb7b101a3ac062
Gabriel de Oliveira <gabrieldeoliveira.gdo23@gmail.com>
2020-04-27 11:49:51 -0300

    (feat) started the rabin karp application

--- Rabin Karp 1404/src/router/routes.js
+++
@@ -1,7 +0,0 @@
-import HomeRoutes from '@/views/home/routes';
-import SearchRoutes from '@/views/search/routes';
-
-export default [
-  ...HomeRoutes,
-  ...SearchRoutes,
-];
--- Rabin Karp 1404/src/views/home/routes.js
+++
@@ -1,9 +0,0 @@
-import Home from './index.vue';
-
-export default [
-  {
-    path: '/home',
-    name: 'home',
-    component: Home,
-  },
-];
