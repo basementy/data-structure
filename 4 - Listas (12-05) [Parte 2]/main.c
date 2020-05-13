90223e2092e0e77beabc1c4c284f6427cf1834d0
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2020-05-12 21:49:05 -0300

    (feat) some initial changes

--- 4 - Listas (12-05) [Parte 2]/main.c
+++ 4 - Listas (12-05) [Parte 2]/main.c
@@ -1,73 +1,67 @@
-// http://hpca23.cse.tamu.edu/taco/utsa-www/ut/utsa/cs1723/lecture4.html
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 typedef struct _student {
-	int		  id;
-	char		name[100];
-	float		gpa;
+	int id;
+	float gpa;
+	char name[100];
 } student;
 
-
-// nó
 typedef struct _node {
-	student		k;
-	struct _node	*next; // ponteiro para o próximo elemento
+	student	k;
+	struct _node *next;
 } node, *list;
 
 
 void insert_list (list *L, student k) {
-	node	*p;
-	p = (node *) malloc (sizeof (node));
+	node *p;
+
+  p = (node *) malloc (sizeof (node));
 	p->k = k;
 	p->next = *L;
 
 	*L = p;
 }
 
-
 student *search_list (list L, int id) {
-	node	*p;
+	node *p;
 
-	for (p=L; p && p->k.id != id; p=p->next);
+	for (p = L; p && p->k.id != id; p = p->next);
 
 	if (p)
-		return &p->k;
+    return &p->k;
 	else
-		return NULL;
+    return NULL;
 }
 
-/* make an empty list */
-
-void create_list (list *L) {
+void create_list(list *L) {
 	*L = NULL;
 }
 
+int main() {
+	list C;
+	student	s, *p;
+	int id;
+	FILE *f;
 
-int main () {
-	list		C;	/* a class of students */
-	student		s, *p;
-	int		id;
-	FILE		*f;
-
-
-
-	f = fopen ("./List.txt", "r");
+	f = fopen ("./archive/list.txt", "r");
 	if (!f) {
 		perror ("List");
 		exit (1);
 	}
 
-	create_list (&C);
+	create_list(&C);
 
 	for (;;) {
 		fscanf (f, "%d %s %f\n", &s.id, s.name, &s.gpa);
+
 		if (feof (f))
       break;
+
 		insert_list (&C, s);
 	}
+
 	fclose (f);
 
 	for (;;) {
@@ -85,5 +79,6 @@ int main () {
 		else
 			printf ("%d\t%s\t%0.2f\n", p->id, p->name, p->gpa);
 	}
+
 	exit (0);
 }
