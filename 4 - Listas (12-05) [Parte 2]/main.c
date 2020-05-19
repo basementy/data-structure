f701e73e07277a9f3b7620218d9345da342053a1
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2020-05-19 16:02:54 -0300

    (refactor) abstracted main file

--- 4 - Listas (12-05) [Parte 2]/main.c
+++ 4 - Listas (12-05) [Parte 2]/main.c
@@ -2,83 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct _student {
-	int id;
-	float gpa;
-	char name[100];
-} student;
-
-typedef struct _node {
-	student	k;
-	struct _node *next;
-} node, *list;
-
-
-void insert_list (list *L, student k) {
-	node *p;
-
-  p = (node *) malloc (sizeof (node));
-	p->k = k;
-	p->next = *L;
-
-	*L = p;
-}
-
-student *search_list (list L, int id) {
-	node *p;
-
-	for (p = L; p && p->k.id != id; p = p->next);
-
-	if (p)
-    return &p->k;
-	else
-    return NULL;
-}
-
-void create_list(list *L) {
-	*L = NULL;
-}
+#include "src/types.c"
+#include "src/utils.c"
 
 int main() {
-	list C;
-	student	s, *p;
-	int id;
-	FILE *f;
+	int studentId;
+  list studentList;
+  student	currentList;
+  FILE *studentsFile;
 
-	f = fopen ("./archive/list.txt", "r");
-	if (!f) {
-		perror ("List");
-		exit (1);
-	}
+  studentsFile = searchFile();
+	createList(&studentList);
 
-	create_list(&C);
+	while (studentsFile) {
+    fscanf(studentsFile, "%d %s %f",
+      &currentList.id,
+      currentList.name,
+      &currentList.grade
+    );
 
-	for (;;) {
-		fscanf (f, "%d %s %f\n", &s.id, s.name, &s.gpa);
-
-		if (feof (f))
+    if (feof(studentsFile))
       break;
 
-		insert_list (&C, s);
+		insertList(&studentList, currentList);
 	}
 
-	fclose (f);
-
-	for (;;) {
-		printf ("Enter student ID, -1 to finish: ");
-		scanf ("%d", &id);
-
-		if (id == -1)
-       break;
-
-		p = search_list (C, id);
-
-
-		if (!p)
-			printf ("ID #%d not found!\n", id);
-		else
-			printf ("%d\t%s\t%0.2f\n", p->id, p->name, p->gpa);
-	}
-
-	exit (0);
+  fclose(studentsFile);
+  mainMenu(studentList);
 }
